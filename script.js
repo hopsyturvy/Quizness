8c485bf2765ebff8ba264bdef9d18636dc3c927b
hopsyturvy <65021990+hopsyturvy@users.noreply.github.com>
2020-05-31 21:15:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+function cheatButton () {
+    document.getElementById("runaway").focus();
+    document.getElementById("runaway").innerHTML="No Cheating!";
+    setTimeout(resetButton, 500)
+
+}
+
+function moveButton () {
+    document.getElementById("runaway").style.left=((50+Math.random()*200))+"px";
+    document.getElementById("runaway").style.top=((10+Math.random()*200))+"px";
+    document.getElementById("runaway").innerHTML="Over here!";
+    setTimeout(resetButton, 500)
+}
+
+function clickButton () {
+
+    let left = +document.getElementById("runaway").style.left.slice(0,-2)
+    console.log(left)
+
+    if (left <300) {
+        left += (200 + Math.random()*100)
+    } else {
+        left -= (200 + Math.random()*100)
+    }
+
+    let top = +document.getElementById("runaway").style.top.slice(0,-2)
+    if (top <200) {
+        top += (100 + Math.random()*100)
+    } else {
+        top -= (100 + Math.random()*100)
+    }
+    
+
+    document.getElementById("runaway").style.left=left+"px";
+    document.getElementById("runaway").style.top=top+"px";
+    document.getElementById("runaway").innerHTML="Too slow!";
+    setTimeout(resetButton, 800)
+}
+
+function resetButton () {
+    document.getElementById("runaway").innerHTML="OK"
+}
+
+function clickEffect(e){
+    var dd = document.createElement("DIV");
+    dd.className="clickEffect";
+    dd.style.top=e.clientY+"px";dd.style.left=e.clientX+"px";
+    document.body.appendChild(dd);
+    dd.addEventListener('animationend',function(){dd.parentElement.removeChild(dd);}.bind(this));
+    }
+
+document.addEventListener('click',clickEffect);
+document.addEventListener("keydown", cheatButton);
+document.getElementById("runaway").addEventListener("mouseover", moveButton);
+document.getElementById("runaway").addEventListener("click", clickButton);
